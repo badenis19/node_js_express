6a8dcc7e71983ea87a0765755fc037d079d271c1
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-10 17:15:32 +0100

    use of Handlebars Templating Engine

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'Jade', condition: true });
+  res.render('index', { title: 'Cool Uh', name: 'Jade', condition: true, anyArray: [4,5,6]});
 });
 
 module.exports = router;
