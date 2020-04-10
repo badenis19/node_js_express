cb9382a2ff8dad0dcdabac1fb2d105c0608a7e7d
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-10 15:42:42 +0100

    PugJS Templating Engine (old JADE)

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/a', function(req, res, next) {
-  res.send('Okkkk');
+router.get('/detail', function(req, res, next) {
+  res.send('Detail');
 });
 
 
