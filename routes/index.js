d75bbc588ef89876b55ed14db8d9d6fb8256ac16
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-04-11 15:48:04 +0100

    get and post

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cool Uh', name: 'Jade', condition: true, anyArray: [4,5,6]});
+  res.render('index', { title: 'I am the Title', name: 'Jade', condition: true, anyArray: [4,5,6]});
 });
+
+router.get('/test/:id', function(req, res, next){
+  res.render('test', {output: req.params.id })
+})
+
+router.post('/test/submit', function(req, res, next){
+  var id = req.body.id;
+  /* redirect to page after post request */
+  res.redirect('/test/' + id);
+})
 
 module.exports = router;
